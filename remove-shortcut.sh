26ad93eefa4aa6896458c58ca9ff1ccecca95f2e
JonasKorte <mscdelirium@gmail.com>
2021-12-09 18:37:59 +0100

    Fixed remove-shortcut.sh

--- remove-shortcut.sh
+++ remove-shortcut.sh
@@ -4,7 +4,7 @@ if [ "$1" == "-help" ]; then
 else
 	if test -f "$HOME/.local/$1.desktop"; then
 		echo "Found file in $HOME/.local!"
-		rm "$HOME/.local/$1.desktop"
+		sudo rm "$HOME/.local/$1.desktop"
 		echo "Success!"
 	else
 		echo "File not found in ~/.local!"
@@ -12,7 +12,7 @@ else
 	
 	if test -f "/usr/share/applications/$1.desktop"; then
 		echo "Found file in /usr/share/applications"
-		rm "/usr/share/applications/$1.desktop"
+		sudo rm "/usr/share/applications/$1.desktop"
 		echo "Success!"
 	else
 		echo "File not found in /usr/share/applications"
