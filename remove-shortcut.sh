0588ee26d50193706bf3ee6860752a54ee914440
JonasKorte <mscdelirium@gmail.com>
2021-12-09 15:26:05 +0100

    Updated remove-shortcut.sh

--- remove-shortcut.sh
+++ remove-shortcut.sh
@@ -1,8 +1,14 @@
 if test -f "~/.local/$1"; then
 	echo "Found file in ~/.local!"
 	rm "~/.local/$1"
+	echo "Success!"
+else
+	echo "File not found in ~/.local!"
 fi
 if test -f "/usr/share/applications/$1"; then
-	echo "Found file in ~/.local!"
+	echo "Found file in /usr/share/applications"
 	rm "/usr/share/applications/$1"
+	echo "Success!"
+else
+	echo "File not found in /usr/share/applications"
 fi
