52395388890ce47e2482c3534f37048d5eda3dc0
JonasKorte <mscdelirium@gmail.com>
2021-12-09 15:29:25 +0100

    Updated remove-shortcut.sh

--- remove-shortcut.sh
+++ remove-shortcut.sh
@@ -1,14 +1,20 @@
-if test -f "~/.local/$1"; then
-	echo "Found file in ~/.local!"
-	rm "~/.local/$1"
-	echo "Success!"
+if [ "$1" == "-help" ]; then
+	echo "Usage: bash remove-shortcut.sh shortcut-name"
+	echo "Replace shortcut-name with the name of the shortcut (without extension)."
 else
-	echo "File not found in ~/.local!"
-fi
-if test -f "/usr/share/applications/$1"; then
-	echo "Found file in /usr/share/applications"
-	rm "/usr/share/applications/$1"
-	echo "Success!"
-else
-	echo "File not found in /usr/share/applications"
+	if test -f "~/.local/$1.desktop"; then
+		echo "Found file in ~/.local!"
+		rm "~/.local/$1.desktop"
+		echo "Success!"
+	else
+		echo "File not found in ~/.local!"
+	fi
+	
+	if test -f "/usr/share/applications/$1.desktop"; then
+		echo "Found file in /usr/share/applications"
+		rm "/usr/share/applications/$1.desktop"
+		echo "Success!"
+	else
+		echo "File not found in /usr/share/applications"
+	fi
 fi
